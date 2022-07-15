6198f63158c1c69b315f6d17b869ac41268f7d50
Not from home <leon.camkiss@gmail.com>
2022-07-15 12:39:15 +0100

    created main TMTY runner

--- src/main/java/com/qa/TMTY/Utils/Generator.java
+++ src/main/java/com/qa/TMTY/Utils/Generator.java
@@ -5,28 +5,33 @@ import java.util.List;
 import java.util.Random;
 
 public class Generator {
-	
+
 	public Long packageIdGen() {
 		List<Long> x = new ArrayList<>();
 		int count = 0;
-		
+		String incato = "";
+
 		do {
 			Long y = new Random().nextLong(12);
 			x.add(y);
 			count++;
 		}while (count < 6);
-		
-		Long f = x.get(0) + x.get(1) + x.get(2) + x.get(3) + x.get(4) + x.get(5);
-		
+
+		for (Long num : x) {
+			incato += num + "";
+
+		}
+		Long f = Long.parseLong(incato);
+
 		return f;
 	}
-	
+
 	public String assignerGen() {
 		String[] nameList = {"Greag","Geoffray","Harriot","Bedroff","Constablew","Dellerwow"};
 		int x = new Random().nextInt(nameList.length);
 		return nameList[x];
 	}
-	
+
 	public String driverGen() {
 		String[] nameList = {"Hophman","Peonit","Shwowman","Cantact","Williamlow","Herfrey"};
 		int x = new Random().nextInt(nameList.length);
